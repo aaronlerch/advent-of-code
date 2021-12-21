7237f4d0c71b2325e8038b30aa2c20c5226ddde7
Aaron Lerch <aaronlerch@gmail.com>
2021-12-20 17:56:56 -0700

    Day 15 - first A* implementation

--- 2021/day15/node.rb
+++ 2021/day15/node.rb
@@ -1,13 +1,10 @@
 class Node
-    attr_reader :x, :y, :value, :children
+    attr_accessor :neighbors
+    attr_reader :x, :y, :value
 
     def initialize(y, x, value)
         @y, @x, @value = y, x, value
         @neighbors = []
-    end
-
-    def add_neighbor(node)
-        @neighbors << node unless @neighbors.include? node
     end
 
     def ==(other)
